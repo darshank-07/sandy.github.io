9e5ed7602e26ed859314e46763454860240722c5
Mayurpujari1 <118499947+Mayurpujari1@users.noreply.github.com>
2023-02-07 19:51:12 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import { AccountBalance, CardTravel, Chat, Favorite, HomeRounded, Settings, SummarizeRounded } from '@mui/icons-material';
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header section */}
+      <Header />
+
+      {/* main container */}
+
+  {/* Bottom container */}
